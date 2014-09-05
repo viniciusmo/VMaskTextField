e3e0a4beca1abf69c3e1bcf6aa5d96efdecee46f
viniciusmo <vinicius.moises.oliveira@gmail.com>
2014-09-05 17:48:31 -0300

    Start project

---
+++ Example/Pods/Pods-Tests-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// FBSnapshotTestCase
+#define COCOAPODS_POD_AVAILABLE_FBSnapshotTestCase
+#define COCOAPODS_VERSION_MAJOR_FBSnapshotTestCase 1
+#define COCOAPODS_VERSION_MINOR_FBSnapshotTestCase 1
+#define COCOAPODS_VERSION_PATCH_FBSnapshotTestCase 0
+
+// Kiwi
+#define COCOAPODS_POD_AVAILABLE_Kiwi
+#define COCOAPODS_VERSION_MAJOR_Kiwi 2
+#define COCOAPODS_VERSION_MINOR_Kiwi 3
+#define COCOAPODS_VERSION_PATCH_Kiwi 0
+
+// VMaskTextField
+#define COCOAPODS_POD_AVAILABLE_VMaskTextField
+#define COCOAPODS_VERSION_MAJOR_VMaskTextField 0
+#define COCOAPODS_VERSION_MINOR_VMaskTextField 1
+#define COCOAPODS_VERSION_PATCH_VMaskTextField 0
+
---
+++ Example/Pods/Pods-VMaskTextField-VMaskTextField-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VMaskTextField_VMaskTextField : NSObject
+@end
+@implementation PodsDummy_Pods_VMaskTextField_VMaskTextField
+@end
