bdf2883f9fec840837e984d954cd560464ecfe6d
viniciusmo <vinicius.moises.oliveira@gmail.com>
2014-09-07 19:02:45 -0300

    Removing tests not used

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -10,29 +10,13 @@ SPEC_BEGIN(InitialTests)
 
 describe(@"My initial tests", ^{
 
-  context(@"will fail", ^{
+  context(@"will pass", ^{
 
       it(@"can do maths", ^{
           [[@1 should] equal:@1];
       });
-
-      it(@"can read", ^{
-          [[@"number" should] equal:@"number"];
-      });
-
   });
-
-  context(@"will pass", ^{
     
-      it(@"can do maths", ^{
-        [[@1 should] beLessThan:@23];
-      });
-    
-      it(@"can read", ^{
-          [[@"team" shouldNot] containString:@"I"];
-      });  
-  });
-  
 });
 
 SPEC_END
